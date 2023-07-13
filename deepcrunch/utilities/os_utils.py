8fd82f0acc59e9592711473b076487e555d7c662
Alan Synn <alan@alansynn.com>
2023-07-13 14:19:02 +0900

    feat: add utils for deepcrunch

--- deepcrunch/utilities/os_utils.py
+++ deepcrunch/utilities/os_utils.py
@@ -1,23 +1,61 @@
-# Copyright 2023 -ignore
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from pathlib import Path
+import importlib
 
 
-# Check if the given string is path and valid
 def is_valid_path(path: str) -> bool:
+    """Check if the given string is path and valid."""
     if Path(path).is_file():
         return True
     else:
         return False
+
+
+def singleton(cls):
+    """Singleton in decorater"""
+    instances = {}
+
+    def _singleton(*args, **kw):
+        """Create a singleton object."""
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+
+    return _singleton
+
+
+class LazyImport:
+    """Lazy import python module until used."""
+
+    def __init__(self, module_name: str):
+        """Initialize LazyImport object.
+
+        Args:
+           module_name (str): The name of the module to be imported later.
+        """
+        self.module_name = module_name
+        self.module = None
+
+    def __getattr__(self, name: str):
+        """Get the attributes of the module by name."""
+        if not self.module:
+            self.module = self._load_module(self.module_name)
+        try:
+            return getattr(self.module, name)
+        except AttributeError:
+            raise ImportError(f"Cannot import {name} from {self.module_name}")
+
+    def _load_module(self, module_name: str):
+        """Load and return a module."""
+        if importlib.util.find_spec(module_name) is not None:
+            return importlib.import_module(module_name)
+        else:
+            raise ImportError(f"Cannot find module {module_name}")
+
+    def __call__(self, *args, **kwargs):
+        """Call the function in that module."""
+        function_name = self.module_name.split(".")[-1]
+        try:
+            function = getattr(self.module, function_name)
+            return function(*args, **kwargs)
+        except AttributeError:
+            raise ImportError(f"Cannot import {function_name} from {self.module_name}")
